42e07b41b99077a14a3a7bcd91602ae5a3ef5f98
KensukeAssist <sugihara.kensuke.zo@mynavi.jp>
2023-05-18 06:26:55 +0000

    フォルダ作成

---
+++ Python_sugihara/inch-t-cm3.py
@@ -0,0 +1,5 @@
+per_inch = 2.54
+inch = 24
+cm = inch * per_inch
+desc = "{0}インチ = {1}センチ".format(inch,cm)
+print(desc)
---
+++ local/if-cafe.py
@@ -0,0 +1,5 @@
+kion = 26
+if kion >= 25:
+    print("氷水をだす")
+else:
+    print("熱いお茶をだす")
