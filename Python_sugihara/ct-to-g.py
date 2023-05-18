fceb2771326aada3fadd5ca0f0c7f506f643a455
KensukeAssist <sugihara.kensuke.zo@mynavi.jp>
2023-05-18 06:37:09 +0000

    ファイル編集

--- Python_sugihara/ct-to-g.py
+++
@@ -1,5 +0,0 @@
-per_ct =0.2
-user = input("何カラットですか?")
-ct = float(user)
-g = ct * per_ct
-desc = "{0}カラット = {1}グラム".format(ct,g)
--- local/if-cafe.py
+++
@@ -1,5 +0,0 @@
-kion = 26
-if kion >= 25:
-    print("氷水をだす")
-else:
-    print("熱いお茶をだす")
