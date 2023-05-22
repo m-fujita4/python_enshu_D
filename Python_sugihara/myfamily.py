d49c7d3a029b816961149bd0051b0f4aed75c78e
KensukeAssist <sugihara.kensuke.zo@mynavi.jp>
2023-05-22 06:40:45 +0000

    myfamily

--- Python_sugihara/myfamily.py
+++ Python_sugihara/myfamily.py
@@ -6,6 +6,7 @@ input2 = int(args[2])
 input3 = str(args[3])
 
 myname_2 = introduce.introfam(input1,input2,input3)
+#インスタンス作成
 print(myname_2.name_out())
 print(myname_2.age_out())
 print(myname_2.cat_out())
