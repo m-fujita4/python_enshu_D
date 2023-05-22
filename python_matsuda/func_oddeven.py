4d283b6d1333d8e2bf65d9fb8182c1edf3010874
matsudamao <matsuda.mao.ga@mynavi.jp>
2023-05-22 03:54:43 +0000

    toi_14

---
+++ python_matsuda/func_oddeven.py
@@ -0,0 +1,19 @@
+import sys
+args = sys.argv
+
+#関数を定義
+def calcvalue(num):
+    #あまりを算出
+    mod = num % 2
+    #あまりの値から奇数偶数判定
+    if mod != 0:
+        print(str(num) + "は奇数")
+    else:
+        print(str(num) + "は偶数")
+
+#引数を代入
+for i in range(1,4):
+    num = int(args[i])
+    calcvalue(num)
+ 
+
