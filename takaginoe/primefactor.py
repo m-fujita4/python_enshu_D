77d069101daa62bc3aca9caf12650d98e6d41e4b
NoeTakagi <takagi.noe.tm@mynavi.jp>
2023-05-22 03:39:42 +0000

    add some practice files

---
+++ takaginoe/primefactor.py
@@ -0,0 +1,19 @@
+import sys
+args = sys.argv
+num = int(args[1])
+
+def primefactor(n):
+    ans = []
+    tmp = n
+    for i in range (2, int(n*0.5//1-1)):
+        if tmp % i == 0:
+            cnt = 0
+            while tmp % i == 0:
+                cnt += 1
+                tmp //= i
+                ans.append(i)
+    if ans==[]:
+        ans.append(n)
+    return ans
+
+print(primefactor(num), end="")
