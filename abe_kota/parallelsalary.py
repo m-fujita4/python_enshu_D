a4489bec366e7f13273bebf341826d992f2865c4
Kota1Abe <abe.kota.eo@mynavi.jp>
2023-05-24 01:20:01 +0000

    a

--- abe_kota/parallelsalary.py
+++ abe_kota/parallelsalary.py
@@ -2,9 +2,9 @@ from func_salary import calcsalary
 
 import sys
 args = sys.argv
-input_salary=int(args[1])
-input_salary=int(args[1])
-input_salary=int(args[1])
+input_salary1=int(args[1])
+input_salary2=int(args[2])
+input_salary3=int(args[3])
 
 #returnで値を二つ返しているのでそれぞれ変数に代入
 pay_amount,tax=calcsalary(input_salary)
