8bbed7c9e5d450255800d8cc939fc9231a27947d
Kota1Abe <abe.kota.eo@mynavi.jp>
2023-05-24 01:22:37 +0000

    ３人分できるように

--- abe_kota/parallelsalary.py
+++ abe_kota/parallelsalary.py
@@ -7,6 +7,11 @@ input_salary2=int(args[2])
 input_salary3=int(args[3])
 
 #returnで値を二つ返しているのでそれぞれ変数に代入
-pay_amount,tax=calcsalary(input_salary)
+pay_amount,tax=calcsalary(input_salary1)
+print(f"給与:{input_salary1}、支給額:{pay_amount}、税額:{tax}")
 
-print(f"給与:{input_salary}、支給額:{pay_amount}、税額:{tax}")
+pay_amount,tax=calcsalary(input_salary2)
+print(f"給与:{input_salary2}、支給額:{pay_amount}、税額:{tax}")
+
+pay_amount,tax=calcsalary(input_salary3)
+print(f"給与:{input_salary3}、支給額:{pay_amount}、税額:{tax}")
